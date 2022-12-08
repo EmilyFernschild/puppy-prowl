8b8bea441f18f81968be9ccb7d628e13ce6ba425
Emily Fernschild <emilyfernschild@gmail.com>
2022-12-08 12:00:38 -0500

    css

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -7,6 +7,7 @@ function Home(){
     return (
         <div>
             <h2 className='welcome'>Welcome to Puppy Prowl!</h2>
+            <h5 className='text' id="welcome-slogan">A walking and training business for all types of dogs!</h5>
             <Carousel infiniteLoop className='main-slide'>
                 <div className='slide'>
                     <img alt="dog walking" src="https://images.unsplash.com/photo-1494947665470-20322015e3a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
